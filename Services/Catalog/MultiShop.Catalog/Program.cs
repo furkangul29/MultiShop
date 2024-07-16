9d1e8ab6a3e6ce829f9d3e485a4657d1173fd416
furkangul29 <gulfurkan@outlook.com.tr>
2024-07-16 20:34:32 +0300

    Poje taslağı oluşturuldu. Yeni mikro servisler eklendi. IdentityServer oluşturuldu. Json Web Token İle bazı güvenlikişlemleri Gerçekleştirildi.

--- Services/Catalog/MultiShop.Catalog/Program.cs
+++ Services/Catalog/MultiShop.Catalog/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Options;
 using MultiShop.Catalog.Services.AboutServices;
 using MultiShop.Catalog.Services.BrandServices;
@@ -17,6 +18,13 @@ using System.Reflection;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.Authority = builder.Configuration["IdentityServerUrl"];
+    opt.Audience = "ResourceCatalog";
+    opt.RequireHttpsMetadata = false;
+});
 
 builder.Services.AddScoped<IStatisticService, StatisticService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
