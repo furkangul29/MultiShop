42e478c69e166b134dc827897d36a72862ca45f2
furkangul29 <gulfurkan@outlook.com.tr>
2024-07-18 15:07:27 +0300

    Basket Mikro Servisi Olusturuldu. Frontend ve Admin temaları yüklendi Frontends kısmına giriş yapıldı ve bu kısım da UILayout tasarlandı.

---
+++ MultiShop.Basket/LoginServices/LoginService.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiShop.Basket.LoginServices
+{
+    public class LoginService : ILoginService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public LoginService(IHttpContextAccessor contextAccessor)
+        {
+            _httpContextAccessor = contextAccessor;
+        }
+
+        //Burada sub key i aracılıgı ile token in icinden UserId yi yakalayacagız.
+        public string GetUserId => _httpContextAccessor.HttpContext.User.FindFirst("sub").Value;
+    }
+}
