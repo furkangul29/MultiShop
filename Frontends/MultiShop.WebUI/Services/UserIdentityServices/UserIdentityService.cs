12297cc0c6a2cf3f27ddfbd02196aa6b08e31a58
furkangul29 <gulfurkan@outlook.com.tr>
2024-08-27 11:02:40 +0300

    Payment icin odeme tasarımı olusturuldu.

--- Frontends/MultiShop.WebUI/Services/UserIdentityServices/UserIdentityService.cs
+++ Frontends/MultiShop.WebUI/Services/UserIdentityServices/UserIdentityService.cs
@@ -12,7 +12,7 @@ namespace MultiShop.WebUI.Services.UserIdentityServices
         }
         public async Task<List<ResultUserDto>> GetAllUserListAsync()
         {
-            var responseMessage = await _httpClient.GetAsync("https://localhost:5001/api/users/GetAllUserList");
+            var responseMessage = await _httpClient.GetAsync("http://localhost:5001/api/users/GetAllUserList");
             var jsonData = await responseMessage.Content.ReadAsStringAsync();
             var values = JsonConvert.DeserializeObject<List<ResultUserDto>>(jsonData);
             return values;
--- Frontends/MultiShop.WebUI/ViewComponents/OrderViewComponents/_OrderSummaryComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/OrderViewComponents/_OrderSummaryComponentPartial.cs
@@ -1,20 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.BasketDtos;
 using MultiShop.WebUI.Services.BasketServices;
 
-namespace MultiShop.WebUI.ViewComponents.OrderViewComponents
+public class _OrderSummaryComponentPartial : ViewComponent
 {
-    public class _OrderSummaryComponentPartial : ViewComponent
+    private readonly IBasketService _basketService;
+
+    public _OrderSummaryComponentPartial(IBasketService basketService)
     {
-        private readonly IBasketService _basketService;
-        public _OrderSummaryComponentPartial(IBasketService basketService)
+        _basketService = basketService;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var basketTotal = await _basketService.GetBasket();
+        var basketItems = basketTotal.BasketItems;
+
+        var shippingCost = CalculateShippingCost(basketItems);
+
+        ViewBag.ShippingCost = shippingCost;
+
+        return View(basketItems);
+    }
+
+    private decimal CalculateShippingCost(List<BasketItemDto> items)
+    {
+        decimal subtotal = items.Sum(item => item.Price * item.Quantity);
+
+        if (subtotal >= 500m)
         {
-            _basketService = basketService;
+            return 0m; // 500 TL ve üzeri siparişlerde kargo bedava
         }
-        public async Task<IViewComponentResult> InvokeAsync()
+        else
         {
-            var basketTotal = await _basketService.GetBasket();
-            var basketItems = basketTotal.BasketItems;
-            return View(basketItems);
+            return 80m; // 500 TL altındaki siparişlerde sabit 80 TL kargo ücreti
         }
     }
 }
