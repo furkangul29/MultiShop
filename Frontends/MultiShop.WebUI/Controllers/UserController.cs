3bf1b5d14c2aa0a1f49473f0f6d26fdadc61ea07
furkangul29 <gulfurkan@outlook.com.tr>
2024-09-25 19:47:54 +0300

    Cargo Customer ile alakalı calısmalar yapıldı örnek veri seti girildi.

--- Frontends/MultiShop.WebUI/Controllers/UserController.cs
+++ Frontends/MultiShop.WebUI/Controllers/UserController.cs
@@ -7,14 +7,17 @@ namespace MultiShop.WebUI.Controllers
     public class UserController : Controller
     {
         private readonly IUserService _userService;
-        public UserController(IUserService userService)
+        private readonly ICargoCustomerService _cargoCustomerService;
+        public UserController(IUserService userService, ICargoCustomerService cargoCustomerService)
         {
             _userService = userService;
+            _cargoCustomerService = cargoCustomerService;
         }
         public async Task<IActionResult> Index()
         {
             var values = await _userService.GetUserInfo();
             return View(values);
         }
+           
     }
 }
