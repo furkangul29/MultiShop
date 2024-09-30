4e3be5b4ee9b258d7755ea905ff6b3260c786e46
furkangul29 <gulfurkan@outlook.com.tr>
2024-09-30 18:31:24 +0300

    SignalR icin Client Side Paketleri eklendi .

--- Frontends/MultiShop.WebUI/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutHeaderComponentPartial.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutHeaderComponentPartial.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Services.CommentServices;
 using MultiShop.WebUI.Services.Interfaces;
 using MultiShop.WebUI.Services.MessageServices;
 using System.Security.Claims;
@@ -9,10 +10,12 @@ namespace MultiShop.WebUI.Areas.Admin.ViewComponents.AdminLayoutViewComponents
     {
         private readonly IMessageService _messageService;
         private readonly IUserService _userService;
-        public _AdminLayoutHeaderComponentPartial(IMessageService messageService, IUserService userService)
+        private readonly ICommentService _commentService;
+        public _AdminLayoutHeaderComponentPartial(IMessageService messageService, IUserService userService, ICommentService commentService)
         {
             _messageService = messageService;
             _userService = userService;
+            _commentService = commentService;
         }
 
         public async Task< IViewComponentResult> InvokeAsync()
@@ -20,6 +23,10 @@ namespace MultiShop.WebUI.Areas.Admin.ViewComponents.AdminLayoutViewComponents
             var user = await _userService.GetUserInfo();
             int messageCount = await _messageService.GetTotalMessageCountByReceiverId(user.Id);
             ViewBag.messageCount = messageCount;
+
+            int totalCommentCount = await _commentService.GetTotalCommentCount();
+            ViewBag.totalCommentCount = totalCommentCount;
+
             return View();
         }
     }
--- Services/SignalRRealTime/MultiShop.SignalRRealTimeApi/Program.cs
+++ Services/SignalRRealTime/MultiShop.SignalRRealTimeApi/Program.cs
@@ -2,6 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder => builder
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .SetIsOriginAllowed((host) => true)
+        .AllowCredentials());
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,6 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
