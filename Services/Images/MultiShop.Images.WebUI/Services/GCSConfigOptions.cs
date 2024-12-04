eaf6306cee9477862c6538d8f196952a5c22152a
furkangul29 <gulfurkan@outlook.com.tr>
2024-12-04 13:07:37 +0300

    Gereksiz Dosya Ve Klasorler Silindi.

--- Services/Images/MultiShop.Images.WebUI/Services/GCSConfigOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MultiShop.Images.Services
-{
-    public class GCSConfigOptions
-    {
-        public string? GCPStorageAuthFile { get; set; }
-        public string? GoogleCloudStorageBucketName { get; set; }
-    }
-}
--- Services/Images/MultiShop.Images.WebUI/Services/ICloudStorageService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MultiShop.Images.Services
-{
-    public interface ICloudStorageService
-    {
-        Task<string> GetSignedUrlAsync(string fileNameToRead, int timeOutInMinutes = 30);
-        Task<string> UploadFileAsync(IFormFile fileToUpload, string fileNameToSave);
-        Task DeleteFileAsync(string fileNameToDelete);
-    }
-}
